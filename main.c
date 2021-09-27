c698d5a8cbb21e6cf5907f085a3a0513ac28313e
Asitha Navaratne <asithan476@gmail.com>
2021-09-27 22:05:36 +0530

    Justified comments

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main(void){
  *	@param Device's address (unsigned char).
  */
 void AWAIT_CALL(unsigned char address){
-	TWAR = address; 									///< Load device's address onto TWAR register.
+	TWAR = address; 							///< Load device's address onto TWAR register.
 	TWCR = (1<<TWEN)|(1<<TWEA);						///< Enable TWI and set TWINT bit to 1. Generate an ACK bit on the TWI bus if the address is received.
 	while(!(TWCR & (1<<TWINT)));						///< Wait while TWI completes current action.
 	while((TWSR & 0xF8) != 0x60);						///< While address has not been received or ACK has not been sent.
@@ -39,11 +39,11 @@ void AWAIT_CALL(unsigned char address){
 void SLAVE_RECEIVE(void){
 	uint8_t i = 0;
 	while((TWSR & 0xF8) != 0xA0){						///< While a STOP condition has not been received.
-		TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);		///< Enable TWI and set TWINT bit to 1. Generate an ACK bit if a data byte has been received.
+		TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);				///< Enable TWI and set TWINT bit to 1. Generate an ACK bit if a data byte has been received.
 		while(!(TWCR & (1<<TWINT)));					///< Wait while TWI completes current action.
 		if((TWSR & 0xF8) != 0xA0){
 			while((TWSR & 0xF8) != 0x80);				///< If a STOP condition has not been received, wait until data is received.
-			data[i] = TWDR;								///< Store received contents in the array.
+			data[i] = TWDR;						///< Store received contents in the array.
 		}
 		i++;
 	}
